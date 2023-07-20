e3f25c522db342aadd88b94465109ed04fda2e5d
AsifAutomation <asifchoudhury991@gmail.com>
2023-07-20 23:22:21 +0530

    First commit for data driven

---
+++ src/test/java/com/way2auto/listeners/CustomListeners.java
@@ -0,0 +1,55 @@
+package com.way2auto.listeners;
+
+import java.io.IOException;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+import com.way2auto.utils.TestUtil;
+
+public class CustomListeners implements ITestListener{
+
+	public void onFinish(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStart(ITestContext arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestFailure(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		Reporter.log("Capture screenshot");
+		try {
+			TestUtil.captureScreenshot();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void onTestSkipped(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestStart(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTestSuccess(ITestResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/test/java/com/way2auto/testcases/BankManagerLoginTest.java
@@ -0,0 +1,27 @@
+package com.way2auto.testcases;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.way2auto.base.TestBase;
+
+public class BankManagerLoginTest extends TestBase{
+	@Test
+	public void bankManagerLoginTest() throws InterruptedException, IOException{
+		
+		//driver.findElement(By.cssSelector(OR.getProperty("bmlBtn_CSS"))).click();
+		//verifyEquals("abc", "xyz");
+		Thread.sleep(3000);
+		log.debug("Inside Login Test");
+		click("bmlBtn_CSS");
+
+		Assert.assertTrue(isElementPresent(By.cssSelector(OR.getProperty("addCustBtn_CSS"))),"Login not successful");
+		
+		log.debug("Login successfully executed");
+		
+		Assert.fail("Login not successful");
+}
+}
